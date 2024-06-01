c0b8a07050bd02a5841894ea1d2695768cc78d8a
Patrick Coelho <100944831+riquelmept@users.noreply.github.com>
2024-06-01 15:24:05 -0300

    Add files via upload

---
+++ day-six.py
@@ -0,0 +1,580 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.patches as mpatches
+
+import numpy as np
+from numpy import random
+
+# melhorar a visualização
+%matplotlib inline
+mpl.style.use('ggplot')
+plt.style.use('fivethirtyeight')
+sns.set(context='notebook', palette='dark', color_codes=True)
+
+#Modelos
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import RandomizedSearchCV
+
+# métricas de avaliação
+from sklearn.metrics import precision_recall_curve, average_precision_score, confusion_matrix, auc, roc_curve
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, accuracy_score, classification_report
+
+# Reamostragem dos dados
+from imblearn.over_sampling import SMOTE, RandomOverSampler
+from imblearn.under_sampling import NearMiss, RandomUnderSampler
+
+# outras
+import time
+import pickle
+import warnings
+from scipy import interp
+from pprint import pprint
+from scipy.stats import norm
+from collections import Counter
+from imblearn.pipeline import Pipeline
+from imblearn.pipeline import make_pipeline as imbalanced_make_pipeline
+
+# melhorar a visualização
+%matplotlib inline
+mpl.style.use('ggplot')
+plt.style.use('fivethirtyeight')
+sns.set(context='notebook', palette='dark', color_codes=True)
+
+# mesagens de warning
+warnings.filterwarnings("ignore")
+
+#Importando DataFrame
+spotify_df = pd.read_csv('https://raw.githubusercontent.com/letpires/7DaysOfCodeSpotifyML/main/dataset.csv', index_col=0)
+spotify_df.head()
+
+#Verificando os dados (quantidade de linhas, colunas e tipo de cada coluna)
+
+spotify_df.info()
+
+#Buscando a descrição dos atributos numéricos
+
+spotify_df.describe()
+
+#Visualizando dimensões do DF
+
+spotify_df.shape
+
+#Visualizando nome das colunas do DF
+
+spotify_df.columns
+
+#Visualizando maiores durações e menores durações de músicas
+
+spotify_df.duration_ms
+
+#Validando quantidade única de Artista e Músicas x Artista
+
+print(spotify_df['artists'].unique().shape)
+print("-"*500)
+print(spotify_df['artists'].value_counts())
+
+#Ordenando em ordem decrescente por valor ausente
+
+(spotify_df.isnull().sum() / spotify_df.shape[0]).sort_values(ascending=False)
+
+#Verificando quantidade de valores ausentes
+
+spotify_df.isnull().sum()
+
+#função que gera uma gráfico de barras com colunas e frequência dos dados faltantes.
+def missing_visualization(df):
+  quant_isnull = df.isnull().sum()
+  columns = df.columns
+  dic = {"colunas":[],"quant_isnull":[]}
+  for coluna,quant in zip(columns,quant_isnull):
+    if quant > 0:
+      dic["colunas"].append(quant)
+      dic["quant_isnull"].append(coluna)
+  df = pd.DataFrame(dic)
+  plt.figure(figsize=(15,5))
+  sns.barplot(x=df["quant_isnull"],y=df["colunas"],data=df, palette="rocket")
+  plt.xticks(rotation=45);
+  
+missing_visualization(spotify_df)
+
+#Quais são as 100 músicas mais populares?
+sorted_df = spotify_df.sort_values('popularity', ascending = False).head(100)
+sorted_df.head()
+
+#Quais são os artistas mais populares?
+artistas_popularidade = spotify_df[['artists', 'popularity']]
+artistas_populares = artistas_popularidade.groupby("artists").mean().sort_values(by='popularity', ascending=False).reset_index()
+
+#Trazendo somente os 5 primeiros
+artistas_populares = artistas_populares.head()
+artistas_populares
+
+#Análise visual de popularidade dos artistas
+
+artistas_populares.plot.barh(color="hotpink") ##visualize the data
+plt.title("TOP 5 Most Popular Artists")
+plt.show()
+
+#Dentre os gêneros musicais, quais são os mais populares?
+
+trend_genre = spotify_df[["track_genre", "popularity"]].sort_values(by="popularity", ascending=False)[:5]
+trend_genre
+
+#Análise visual dentre os gêneros mais populares
+
+sns.barplot(x="track_genre",y="popularity", data=trend_genre, color = 'hotpink')
+plt.title("Top trending genre")
+plt.show()
+
+#Analisando as músicas mais longas
+
+long_songs = spotify_df[["track_name", "duration_ms"]].sort_values(by="duration_ms", ascending=False)[:5]
+long_songs
+
+#Análise visual das músicas mais longas comparando sua dimensão em milissegundos
+
+sns.barplot(x="duration_ms", y="track_name", data= long_songs, color = 'hotpink')
+plt.title("Top 5 Longest songs")
+plt.show()
+
+#Analisando as músicas mais "dançáveis" de acordo com o DF
+
+danceable = spotify_df[["track_name", "artists", "danceability"]].sort_values(by="danceability", ascending=False)[:5]
+danceable 
+
+#Análise visual das músicas por danceability
+
+colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99', '#ffb6c1']
+
+plt.pie(x="danceability", data=danceable, autopct='%1.2f%%', labels=danceable.track_name, colors = colors)
+plt.title("Top 5 Most Danceable Songs")
+plt.show()
+
+spotify_df.describe()
+
+corr_table = spotify_df.select_dtypes(include=[np.number]).corr(method="pearson")
+corr_table
+
+#Análise Visual da correlação
+
+#Plotando a tabela de correlação usando o Seaborn.
+plt.figure(figsize=(16,4))
+sns.heatmap(corr_table, annot=True, fmt=".1g")
+plt.title("Correlation Heatmap between variables")
+plt.show() #mostrando o plot
+
+#Sequência Dia2
+
+#Removendo itens duplicados
+
+spotify_df = spotify_df.drop_duplicates()
+spotify_df.head()
+
+# Criando classes de popularidade
+spotify_df[spotify_df["popularity"] >=80]
+
+conditionlist = [
+    (spotify_df['popularity'] >= 80) ,
+    (spotify_df['popularity'] <80)]
+
+choicelist = [1,0]
+spotify_df['pop_classe'] = np.select(conditionlist, choicelist, default='Not Specified')
+spotify_df['pop_classe'] = spotify_df['pop_classe'].astype(int)
+
+spotify_df = spotify_df.dropna() #removendo valores nulos
+spotify_df.columns
+
+spotify_df = spotify_df.drop(columns=['popularity'])
+spotify_df.info()
+
+#Mantendo somente colunas quantitativas e que são importantes para o modelo
+
+df_quantitative = spotify_df
+cols_to_drop = []
+for column in spotify_df:
+    if spotify_df[column].dtype == 'object':
+        cols_to_drop.append(column)
+df_quantitative = spotify_df.drop(columns=cols_to_drop)
+
+print(f"Tamanho da base de dados: {df_quantitative.shape}")
+
+df_quantitative.head()
+
+df_quantitative = df_quantitative.drop(columns=['explicit'])
+df_quantitative.info()
+
+# Normalizando os dados, deixando na mesma escala
+df_quantitative_nm=(df_quantitative-df_quantitative.min())/(df_quantitative.max()-df_quantitative.min())
+df_quantitative_nm.head()
+
+#Dia3
+
+#Separando os dados para treino e teste
+df_train, df_test = sklearn.model_selection.train_test_split(df_quantitative_nm, test_size=0.2, random_state=42, shuffle=True)
+
+#Visualizando as proporções da variável alvo
+df_train.pop_classe.value_counts(normalize=True)
+
+#Visualizando as proporções da variável alvo
+df_test.pop_classe.value_counts(normalize=True)
+
+#Dividindo X e Y (X para obter Y)
+X = df_train.drop('pop_classe', axis=1)
+y = df_train.pop_classe
+
+# separando os dados mantendo a porcentagem de amostras em cada classe
+StratifKfold = sklearn.model_selection.StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+#Laço para separar os dados em treino e validação
+for train_index, val_index in StratifKfold.split(X, y):
+
+    X_train, X_val = X.iloc[train_index], X.iloc[val_index]
+    y_train, y_val = y.iloc[train_index], y.iloc[val_index]
+    
+#Verificando as proporções da classe 1 na divisão
+print(f'Dimensões: {X_train.shape, X_val.shape, y_train.shape, y_val.shape}\n')
+print(f"Proporção do df_train para classe=1: {round(len(df_train[df_train.pop_classe==1]) / df_train.shape[0], 4)}\n")
+print(f"Proporção de X_train para classe=1:  {round(len(y_train[y_train==1]) / X_train.shape[0], 4)}")
+print(f"Proporção de X_val para classe=1:    {round(len(y_val[y_val==1]) / X_val.shape[0], 4)}")
+
+# Instanciando o modelo
+logReg = LogisticRegression()
+
+# Treinando o modelo
+logReg.fit(X_train, y_train)
+
+# Prevendo nos dados de treino
+y_pred_base_train = logReg.predict(X_train)
+
+# Prevendo nos dados de validação
+y_pred_base_val = logReg.predict(X_val)
+
+logReg.coef_.tolist()[0]
+
+df_coef = df_quantitative_nm.drop(columns='pop_classe')
+df_coef.columns
+
+data = {
+    "Features": df_coef.columns,
+    "Coef": logReg.coef_.tolist()[0]
+}
+
+df = pd.DataFrame(data)
+df
+
+print('Nos dados de TREINO:')
+
+print('---' * 20)
+print('Modelo:    Regressão Logística (baseline)\n')
+print(f"accuracy:  {accuracy_score(y_train, y_pred_base_train)}")
+print(f"precision: {precision_score(y_train, y_pred_base_train)}")
+print(f"recall:    {recall_score(y_train, y_pred_base_train)}")
+print(f"f1:        {f1_score(y_train, y_pred_base_train)}")
+print()
+print('---' * 20)
+print('---' * 20)
+print()
+print('Nos dados de VALIDAÇÃO:')
+print('---' * 20)
+print('Modelo:    Regressão Logística (baseline)\n')
+print(f"accuracy:  {accuracy_score(y_val, y_pred_base_val)}")
+print(f"precision: {precision_score(y_val, y_pred_base_val)}")
+print(f"recall:    {recall_score(y_val, y_pred_base_val)}")
+print(f"f1:        {f1_score(y_val, y_pred_base_val)}")
+print('---' * 20)
+
+# Dicionário dos classificadores
+classifiers = {
+    "LogisiticRegression": LogisticRegression(),
+    "KNearest": KNeighborsClassifier(),
+    "DecisionTreeClassifier": DecisionTreeClassifier(), 
+    "Random Forest": RandomForestClassifier()
+}
+
+# instanciando o método
+undersample = NearMiss()
+
+# transformando o conjunto de dados
+X_under, y_under = undersample.fit_resample(X_train, y_train)
+
+# resumindo a distribuição da nova classe
+counter = Counter(y_under)
+print(counter)
+
+for nome, model in classifiers.items():
+    
+    model.fit(X_under, y_under)
+    y_pred = model.predict(X_val)
+    
+    print(f'Modelo:    {nome}\n')
+    print(f"accuracy:  {accuracy_score(y_val, y_pred)}")
+    print(f"precision: {precision_score(y_val, y_pred)}")
+    print(f"recall:    {recall_score(y_val, y_pred)}")
+    print(f"f1:        {f1_score(y_val, y_pred)}")
+    print('---' * 20)
+    print('---' * 20)
+    
+# instanciando o método
+oversample = RandomOverSampler(sampling_strategy='minority')
+
+# transformando o conjunto de dados
+X_over, y_over = oversample.fit_resample(X_train, y_train)
+
+# resumindo a distribuição da nova classe
+counter = Counter(y_over)
+print(counter)
+
+for nome, model in classifiers.items():
+    
+    model.fit(X_over, y_over)
+    y_pred = model.predict(X_val)
+    
+    print(f'Modelo:    {nome}\n')
+    print(f"accuracy:  {accuracy_score(y_val, y_pred)}")
+    print(f"precision: {precision_score(y_val, y_pred)}")
+    print(f"recall:    {recall_score(y_val, y_pred)}")
+    print(f"f1:        {f1_score(y_val, y_pred)}")
+    print('---' * 20)
+    print('---' * 20)
+    
+# instanciando o método
+oversample_smote = SMOTE(sampling_strategy='minority')
+
+# transformando o conjunto de dados
+X_smote, y_smote = oversample_smote.fit_resample(X_train, y_train)
+
+# resumindo a distribuição da nova classe
+counter = Counter(y_smote)
+print(counter)
+
+for nome, model in classifiers.items():
+    
+    model.fit(X_smote, y_smote)
+    y_pred = model.predict(X_val)
+    
+    print(f'Modelo:    {nome}\n')
+    print(f"accuracy:  {accuracy_score(y_val, y_pred)}")
+    print(f"precision: {precision_score(y_val, y_pred)}")
+    print(f"recall:    {recall_score(y_val, y_pred)}")
+    print(f"f1:        {f1_score(y_val, y_pred)}")
+    print('---' * 20)
+    print('---' * 20)
+    
+# transformando o conjunto de dados
+over = RandomOverSampler(sampling_strategy=0.1)
+under = RandomUnderSampler(sampling_strategy=0.5)
+
+# instanciando um pipeline com os dois métodos
+pipeline = Pipeline(steps=[('o', over), ('u', under)])
+
+# transformando o conjunto de dados
+X_under_over, y_under_over = pipeline.fit_resample(X_train, y_train)
+
+# resumindo a distribuição da nova classe
+counter = Counter(y_under_over)
+print(counter)
+
+for nome, model in classifiers.items():
+    
+    model.fit(X_under_over, y_under_over)
+    y_pred = model.predict(X_val)
+    
+    print(f'Modelo:    {nome}\n')
+    print(f"accuracy:  {accuracy_score(y_val, y_pred)}")
+    print(f"precision: {precision_score(y_val, y_pred)}")
+    print(f"recall:    {recall_score(y_val, y_pred)}")
+    print(f"f1:        {f1_score(y_val, y_pred)}")
+    print('---' * 20)
+    print('---' * 20)
+    
+# transformando os dados
+X_train_array = X_train.values
+X_val_array = X_val.values
+y_train_array = y_train.values
+y_val_array = y_val.values
+
+#HIPERPARÂMETROS PARA RANDOM FOREST
+
+# número de árvores
+n_estimators = [3,5,10,100]
+
+# número de features para considerar em cada split
+max_features = [3,4,5,6,None]
+
+# número máximo de níveis da árvore
+max_depth = [3,4,5,6,None]
+
+# pesos para cada classe
+class_weight = ['balanced',None]
+
+# colocando os parametros definidos em um dicionário
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'class_weight': class_weight}
+
+# exibindo os parâmetros
+pprint(random_grid)
+
+# definindo listas vazias para receber os resultados da validação cruzada
+accuracy_lst_rf = []
+precision_lst_rf = []
+recall_lst_rf = []
+f1_lst_rf = []
+auc_lst_rf = []
+
+
+# instanciando o RandomizedSearchCV para validação cruzada com o grid de parametros
+rf_grid = RandomizedSearchCV(RandomForestClassifier(), random_grid, random_state=42)
+
+# iniciando o tempo e contador para o loop
+start = time.time()
+count = 0
+
+# looping para a validação cruzada
+for train, test in StratifKfold.split(X_train_array, y_train_array):
+    
+    # instanciando o pipeline com técnica de reamostragem
+    pipeline_rf = imbalanced_make_pipeline(RandomOverSampler(sampling_strategy='minority'), rf_grid)
+    
+    # executando o pipeline para treinar o modelo
+    model_rf = pipeline_rf.fit(X_train_array[train], y_train_array[train])
+    
+    # identificando os melhores parâmetros no treinamento
+    best_est_rf = rf_grid.best_estimator_
+    
+    # realizando as previsões com o resultado do treinamento
+    prediction_rf = best_est_rf.predict(X_train_array[test])
+    
+    # armazenando os resultados
+    accuracy_lst_rf.append(pipeline_rf.score(X_train_array[test], y_train_array[test]))
+    precision_lst_rf.append(precision_score(y_train_array[test], prediction_rf))
+    recall_lst_rf.append(recall_score(y_train_array[test], prediction_rf))
+    f1_lst_rf.append(f1_score(y_train_array[test], prediction_rf))
+    auc_lst_rf.append(roc_auc_score(y_train_array[test], prediction_rf))
+    
+    # identificando em qual loop está
+    count = count + 1
+    print(f"kfold nº: {count}")
+    
+    # medindo o tempo de cada loop
+    intervalo = time.time()
+    print(f"tempo intermediário: {(intervalo - start)/60:.1f} minutos")
+    print()
+
+print('---' * 20)
+print(f"melhores estimadores para RF: {rf_grid.best_params_}")
+
+# imprimindo a média dos resultados da validação cruzada
+print('---' * 20)
+print('Resultados')
+print(f"accuracy:  {np.mean(accuracy_lst_rf)}")
+print(f"precision: {np.mean(precision_lst_rf)}")
+print(f"recall:    {np.mean(recall_lst_rf)}")
+print(f"f1:        {np.mean(f1_lst_rf)}")
+print(f"AUC:       {np.mean(auc_lst_rf)}")
+print('---' * 20)
+
+# imprimindo o tempo total da validação cruzada
+end = time.time()
+print(f"Tempo do loop para validação cruzada: {(end - start)/60:.1f} minutos")
+
+# criando uma função para plotar o gráfico
+def draw_cv_roc_curve(classifier, cv, X, y, title='ROC Curve'):
+    # definindo algumas listas vazias para receber o resultado
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+    
+    # definindo o contador
+    i = 0
+    
+    # loop ara validação cruzada
+    for train, test in cv.split(X, y):
+        
+        # executando a função e armazenando as probabilidade dos resultados
+        probas_ = classifier.fit(X.iloc[train], y.iloc[train]).predict_proba(X.iloc[test])
+        
+        # computando a curva ROC e AUC
+        fpr, tpr, thresholds = roc_curve(y.iloc[test], probas_[:, 1])
+        tprs.append(interp(mean_fpr, fpr, tpr))
+        tprs[-1][0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        aucs.append(roc_auc)
+        
+        # plotando a curva ROC da validação cruzada em individualmente
+        plt.plot(fpr, tpr, lw=1, alpha=0.3,
+                 label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
+
+        i += 1
+    
+    # plotando a linha diagonal como referência da taxa de 50/50
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+             label='Luck', alpha=.8)
+
+    # calculando as médias e desvio padrão
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    
+    # plotando curva ROC com a média dos resultados
+    plt.plot(mean_fpr, mean_tpr, color='b',
+             label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+             lw=2, alpha=.8)
+
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                     label=r'$\pm$ 1 std. dev.')
+    
+    # customizando o gráfico
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate', fontsize=12)
+    plt.ylabel('True Positive Rate', fontsize=12)
+    plt.title(title)
+    plt.legend(loc="lower right")
+    
+    # exibindo o gráfico
+    plt.show()
+    
+# executando função e plotando a Curva ROC AUC
+draw_cv_roc_curve(best_est_rf, StratifKfold, X_train, y_train, title='ROC Curve')
+
+# separando novamente os dados
+X_all = df_train.drop('pop_classe', axis=1)
+y_all = df_train.pop_classe
+
+# instanciando o modelo final com os parametros encontrados na validação cruzada
+final_model = rf_grid.best_estimator_
+
+# definindo o pipeline para reamostragem oversampling e o modelo final
+pipeline = imbalanced_make_pipeline(RandomOverSampler(sampling_strategy='minority'), final_model)
+
+# treinando o modelo com os parametros ajustados com todos os dados
+model = pipeline.fit(X_all, y_all)
+
+model.steps[1]
+
+lista = model.steps[1][1].feature_importances_
+
+print(lista.tolist())
+
+# lista de nomes
+nomes = df_quantitative_nm.columns
+
+plt.barh(df_quantitative_nm.drop(columns=['pop_classe']).columns.tolist(), lista.tolist())
+
+plt.show()
